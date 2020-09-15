934376d5f36fb3605819440405d447391eac75bd
Burbulis <onehalf3554@mail.ru>
2020-09-15 16:28:08 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,40 +2,24 @@
 
 //
 https://gist.github.com/hasherezade/87158b926e33418f5d3b0a0026d0ccc2
-
+https://coderoad.ru/3801517/%D0%9A%D0%B0%D0%BA-%D0%BF%D0%B5%D1%80%D0%B5%D1%87%D0%B8%D1%81%D0%BB%D0%B8%D1%82%D1%8C-%D0%BC%D0%BE%D0%B4%D1%83%D0%BB%D0%B8-%D0%B2-64-%D0%B1%D0%B8%D1%82%D0%BD%D0%BE%D0%BC-%D0%BF%D1%80%D0%BE%D1%86%D0%B5%D1%81%D1%81%D0%B5-%D0%B8%D0%B7-32-%D0%B1%D0%B8%D1%82%D0%BD%D0%BE%D0%B3%D0%BE-%D0%BF%D1%80%D0%BE%D1%86%D0%B5%D1%81%D1%81%D0%B0-WOW
 
 #include "stdafx.h"
-
 #include <cassert>
-
 #include <vector>
-
 #include <string>
-
 #include <sstream>
-
 #include <functional>
-
 #include <dos.h>
-
 #include <stdio.h>
-
 #include <io.h>
-
 #include <direct.h>
-
 #include <stdlib.h>
-
 #include <iostream>
-
 #include <istream>
-
 #include <ostream>
-
 #include <boost/asio.hpp>
-
 #include <boost/bind.hpp>
-
 #include <boost/thread/mutex.hpp>
 
 
